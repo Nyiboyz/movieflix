3ff15a31987452a2a306fd96f4a0d5d290ca1da4
Nyiboyz <96110141+Nyiboyz@users.noreply.github.com>
2026-09-26 01:51:54 +0630

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,20 @@
+// Paste your unique Firebase config here
+const firebaseConfig = {
+   apiKey: "AIzaSyA3el9LwR5CH2NJxwhYfuHbX0WqGuNmBdo",
+  authDomain: "movieflix-2b245.firebaseapp.com",
+  databaseURL: "https://movieflix-2b245-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "movieflix-2b245",
+  storageBucket: "movieflix-2b245.firebasestorage.app",
+  messagingSenderId: "340751379577",
+  appId: "1:340751379577:web:5fe945cd8f123a21bcb8a8",
+  measurementId: "G-B0ZJTX3HPE"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
 const CONFIG = {
-    SUPABASE_URL: 'https://rzqxvdreggozdhydhdlv.supabase.co',
-    SUPABASE_ANON_KEY: 'sb_publishable_WJhaNLpg8sPYQde4n7ksxA_pmGD_R6C',
-    BOT_USERNAME: 'MovieFlix_Delivery_Bot' // e.g., MovieFlix_Delivery_Bot (No @ symbol)
+    BOT_USERNAME: 'MovieFlix_Delivery_Bot' // e.g., MovieFlix_Delivery_Bot
 };
